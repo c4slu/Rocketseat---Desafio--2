2e9e6c360fc6fc31922048d85873c9338c403807
c4slu <lucasnascimento.brn@gmail.com>
2023-01-11 17:28:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function add()
+{
+    document.getElementById("gif").classList.add("active")
+    document.getElementById("x").classList.add("active")
+    document.getElementById("icon").classList.add("active")
+}
+function remove()
+{
+    document.getElementById("gif").classList.remove("active")
+    document.getElementById("x").classList.remove("active")
+    document.getElementById("icon").classList.remove("active")
+}
